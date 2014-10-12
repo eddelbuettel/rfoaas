5e61f08978129b529b1ee9a57b9560fc667cad00
Dirk Eddelbuettel <edd@debian.org>
2014-10-11 21:54:38 -0500

    version 0.0.3 with expanded FOAAS coverage

--- R/foaas.R
+++ R/foaas.R
@@ -7,21 +7,24 @@
     res
 }
 
-off         <- function(name, from) { .foaas("off", name, from) }
-you         <- function(name, from) { .foaas("you", name, from) }
-this        <- function(from)       { .foaas("this", from) }
-that        <- function(from)       { .foaas("that", from) }
-everything  <- function(from)       { .foaas("everything", from) }
-everyone    <- function(from)       { .foaas("everyone", from) }
-donut       <- function(name, from) { .foaas("donut", name, from) }
-shakespeare <- function(name, from) { .foaas("shakespeare", name, from) }
-linus       <- function(name, from) { .foaas("linus", name, from) }
-king        <- function(name, from) { .foaas("king", name, from) }
-pink        <- function(name)       { .foaas("pink", name) }
-life        <- function(name)       { .foaas("life", name) }
-chainsaw    <- function(name, from) { .foaas("chainsaw", name, from) }
-thing       <- function(name, from) { .foaas(name, from) }
-thanks      <- function(from)       { .foaas("thanks", from) }
-flying      <- function(from)       { .foaas("flying", from) }
-fascinating <- function(from)       { .foaas("fascinating", from) }
-#madison     <- function(name, from) { .foaas("madison", name, from) }
+off         <- function(name, from)            { .foaas("off", name, from) }
+you         <- function(name, from)            { .foaas("you", name, from) }
+this        <- function(from)                  { .foaas("this", from) }
+that        <- function(from)                  { .foaas("that", from) }
+everything  <- function(from)                  { .foaas("everything", from) }
+everyone    <- function(from)                  { .foaas("everyone", from) }
+donut       <- function(name, from)            { .foaas("donut", name, from) }
+shakespeare <- function(name, from)            { .foaas("shakespeare", name, from) }
+linus       <- function(name, from)            { .foaas("linus", name, from) }
+king        <- function(name, from)            { .foaas("king", name, from) }
+pink        <- function(name)                  { .foaas("pink", name) }
+life        <- function(name)                  { .foaas("life", name) }
+chainsaw    <- function(name, from)            { .foaas("chainsaw", name, from) }
+thing       <- function(name, from)            { .foaas(name, from) }
+thanks      <- function(from)                  { .foaas("thanks", from) }
+flying      <- function(from)                  { .foaas("flying", from) }
+fascinating <- function(from)                  { .foaas("fascinating", from) }
+madison     <- function(name, from)            { .foaas("madison", name, from) }
+cool        <- function(from)                  { .foaas("cool", from) }
+field       <- function(name, from, reference) { .foaas("field", name, from, reference) }
+nugget      <- function(name, from)            { .foaas("nugget", name, from) }
